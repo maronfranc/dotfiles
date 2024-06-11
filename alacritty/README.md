51ed238d3ecfa112a3ccfa14102ddd151ab72178
Máron Franca <maron.franca@gmail.com>
2024-06-11 19:29:29 -0300

    [alacritty] add configuration.

---
+++ alacritty/README.md
@@ -0,0 +1,4 @@
+# Alacritty config
+
+## Load
+Copy or symlink alacritty to `~/.config/alacritty/alacritty.yml`
--- tmux/load.sh
+++ tmux/load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo rm -f /etc/tmux.conf
+# sudo ln -s ./tmux.conf /etc/tmux.conf
 sudo cp ./tmux.conf /etc/tmux.conf
 tmux kill-server
-# sudo ln -s ./tmux.conf /etc/tmux.conf
