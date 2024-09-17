95b91e659c320c8605ee8079df6c760a41acc392
Máron <maron.franca@gmail.com>
2024-09-16 22:23:41 -0300

    [setup][neovim] update install script.

--- setup_dev_environment/install_neovim.sh
+++ setup_dev_environment/install_neovim.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+PROGRAMS_DIR="~/zzz_programs"
 # Install neovim lastest
 sudo apt remove neovim --purge
 sudo apt autoremove autoclean clean
@@ -8,7 +11,12 @@ sudo apt install git build-essential cmake git pkg-config libtool g++ libunibili
 ninja-build gettext libtool libtool-bin autoconf automake unzip curl doxygen lua-term lua-term-dev luarocks
 
 # Build
-git clone https://github.com/neovim/neovim
-cd neovim || exit
+git clone https://github.com/neovim/neovim "${PROGRAMS_DIR}/neovim"
+cd "${PROGRAMS_DIR}/neovim" || exit
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+
+# Install "xclip" to enable copy and paste from neovim.
+sudo apt install xclip
+# Install trash-cli to neo-tree plugin run send to trash command.
+sudo apt install trash-cli
