0c313d4da06f3d020164ef29da63a1d3470076b0
Máron <maron.franca@gmail.com>
2025-12-30 10:45:48 -0300

    [tmux] update conf and move install file.

---
+++ setup_dev_environment/tmux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'Installing TMUX...'
+sudo apt install tmux
+echo 'Installing TPM: Tmux Plugin Manager...'
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ 
+echo 'TPM installed! Execute the following steps:'
+echo '1. Run load.sh;'
+echo '2. Enter Tmux and press I(capital i) to install tmux.conf dependencies;'
+echo '3. Reload Tmux server or kill it and reopen.`tmux kill-server &> /dev/null`.'
