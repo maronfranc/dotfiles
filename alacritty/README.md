747167247217e45e4892bb7dc781015bcc719c09
Máron <maron.franca@gmail.com>
2025-12-30 10:33:51 -0300

    [alacritty] update to toml configuration.

--- alacritty/README.md
+++
@@ -1,4 +0,0 @@
-# Alacritty config
-
-## Load
-Copy or symlink alacritty to `~/.config/alacritty/alacritty.yml`
---
+++ alacritty/load.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+alacritty_dir=~/.config/alacritty
+alacritty_file="$alacritty_dir"/alacritty.toml
+
+mkdir -p $alacritty_dir
+rm -rf $alacritty_file
+
+ln -s $(pwd)/alacritty.toml $alacritty_file
+fc-cache -fv
+
+echo "RUNNED: \n 1. ln -s $(pwd)/alacritty.toml $alacritty_file"
+echo "- 1. Link file: ln -s $(pwd)/alacritty.toml $alacritty_file"
+echo "- 2. Update cache: fc-cache -fv"
+echo "- 3. Search if hack fonts are loaded: fc-list | grep -i hack"
+echo -e "4. Check if these icons( \uf115  \uf07c  \uf0c7 ) will work."
