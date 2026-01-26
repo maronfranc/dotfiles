28723b0b752e9b9143349ceeaf32f2120a0cf46f
Máron <maron.franca@gmail.com>
2026-01-26 10:46:33 -0300

    [custom_bash] improve source_files.sh with `../` path.

--- custom_bash/source_files.sh
+++ custom_bash/source_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Global vars being used in other bash files
-# DOTFILE_DIR="$HOME/_dotfiles"
-DOTFILE_DIR="$HOME/000/000_github/dotfiles"
+# DOTFILE_DIR is "../"
+DOTFILE_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
 BASH_DIR="$DOTFILE_DIR/custom_bash"
 SOURCE_DIR="$BASH_DIR/sourced_files"
 
@@ -13,8 +13,8 @@ for file_path in "$SOURCE_DIR"/*; do
     fi
 done
 
-if [ -d "$SOURCE_DIR/custom" ]; then
-    for file_path in "$SOURCE_DIR"/custom/*; do
+if [ -d "$SOURCE_DIR/dont_commit" ]; then
+    for file_path in "$SOURCE_DIR"/dont_commit/*; do
         if [ -f "$file_path" ]; then
             source "$file_path"
         fi
